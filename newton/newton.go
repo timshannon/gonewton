53bd0a085043d9ab1f88ece19940c1c64b6846d4
Tim Shannon <shannon.timothy@gmail.com>
2012-09-11 16:30:40 -0500

    Cleanup

--- newton/newton.go
+++ newton/newton.go
@@ -1,7 +1,7 @@
 package newton
 
 /*
-#cgo   linux LDFLAGS: -L/usr/local/lib -pthread -lNewton -lstdc++
+#cgo   linux LDFLAGS: -L/usr/local/lib -lNewton -lstdc++
 #include "Newton.h"
 #include <stdlib.h>
 */
