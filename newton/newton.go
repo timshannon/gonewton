96d83615818a5b9258b498815a186618dd663d31
Tim Shannon <shannon.timothy@gmail.com>
2012-10-04 16:19:48 -0500

    Several more functions, split materials out to separate source file.

--- newton/newton.go
+++ newton/newton.go
@@ -29,10 +29,6 @@ type Joint struct {
 	handle *C.NewtonJoint
 }
 
-type Material struct {
-	handle *C.NewtonMaterial
-}
-
 type Collision struct {
 	handle *C.NewtonCollision
 }
@@ -106,22 +102,10 @@ func (w *World) ConstraintCount() int {
 	return int(C.NewtonWorldGetConstraintCount(w.handle))
 }
 
-func (w *World) CreateMaterialGroupID() int {
-	return int(C.NewtonMaterialCreateGroupID(w.handle))
+func (w *World) FirstBody() *Body {
+	return &Body{C.NewtonWorldGetFirstBody(w.handle)}
 }
 
-func (w *World) DefaultMaterialGroupID() int {
-	return int(C.NewtonMaterialGetDefaultGroupID(w.handle))
-}
-
-func (w *World) DestroyAllMaterialGroupID() {
-	C.NewtonMaterialDestroyAllGroupID(w.handle)
-}
-
-func (w *World) MaterialUserData(matid0, matid1 int) *interface{} {
-	return (*interface{})(C.NewtonMaterialGetUserData(w.handle, C.int(matid0), C.int(matid1)))
-}
-
-func (w *World) SetMaterialSurfaceThickness(matid0, matid1 int, thickness float32) {
-	C.NewtonMaterialSetSurfaceThickness(w.handle, C.int(matid0), C.int(matid1), C.dFloat(thickness))
+func (w *World) NextBody(curBody *Body) *Body {
+	return &Body{C.NewtonWorldGetNextBody(w.handle, curBody.handle)}
 }
