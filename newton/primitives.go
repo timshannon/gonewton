1353dc59ca662eef5e8e24dabe1f575ff04680a9
Tim Shannon <tim@townsourced.com>
2012-11-06 15:51:39 -0600

    Finished the last method.  Everything I wanted to get in is in.  Now onto testing.

--- newton/primitives.go
+++ newton/primitives.go
@@ -3,6 +3,7 @@ package newton
 /*
 #cgo   linux LDFLAGS: -L/usr/local/lib -lNewton -lstdc++
 #include "Newton.h"
+#include "callback.h"
 #include <stdlib.h>
 */
 import "C"
@@ -451,25 +452,207 @@ func (m *Mesh) IndirectVertexStreams(vertexStrideInByte int, vertex []float32, v
 
 }
 
-/*
-//skip mesh material handle for now. At least until I understand it
 type MeshHandle struct {
 	handle unsafe.Pointer
-	mesh   *Mesh
 }
 
+//I might be able to abstract this a bit more, but I can't wrap my head around
+// the difference between a mesh material, and a mesh material material
+
 func (m *Mesh) BeginHandle() *MeshHandle {
-	return &MeshHandle{C.NewtonMeshBeginHandle(m.handle), m}
+	return &MeshHandle{C.NewtonMeshBeginHandle(m.handle)}
 }
 
 func (m *Mesh) EndHandle(handle *MeshHandle) {
 	C.NewtonMeshEndHandle(m.handle, handle.handle)
 }
-func (m *Mesh) FirstMaterial(handle MeshHandle) int {
-	return int(C.NewtonMeshFirstMaterial(m.handle, unsafe.Pointer(handle)))
+func (m *Mesh) FirstMaterial(handle *MeshHandle) int {
+	return int(C.NewtonMeshFirstMaterial(m.handle, handle.handle))
 }
 
-func (m *Mesh) NextMaterial(handle MeshHandle, materialID int) int {
-	return int(C.NewtonMeshNextMaterial(m.handle, unsafe.Pointer(handle), C.int(materialID)))
+func (m *Mesh) NextMaterial(handle *MeshHandle, materialID int) int {
+	return int(C.NewtonMeshNextMaterial(m.handle, handle.handle, C.int(materialID)))
 }
-*/
+
+func (m *Mesh) MaterialGetMaterial(handle *MeshHandle, materialId int) int {
+	return int(C.NewtonMeshMaterialGetMaterial(m.handle, handle.handle, C.int(materialId)))
+}
+
+func (m *Mesh) MaterialGetIndexCount(handle *MeshHandle, materialId int) int {
+	return int(C.NewtonMeshMaterialGetIndexCount(m.handle, handle.handle, C.int(materialId)))
+}
+
+func (m *Mesh) MaterialGetIndexStream(handle *MeshHandle, materialId int, index []int) {
+	C.NewtonMeshMaterialGetIndexStream(m.handle, handle.handle, C.int(materialId),
+		(*C.int)(unsafe.Pointer(&index[0])))
+}
+
+func (m *Mesh) MaterialGetIndexStreamShort(handle *MeshHandle, materialId int, index []int16) {
+	C.NewtonMeshMaterialGetIndexStreamShort(m.handle, handle.handle, C.int(materialId),
+		(*C.short)(unsafe.Pointer(&index[0])))
+}
+
+func (m *Mesh) CreateFirstSingleSegment() *Mesh {
+	return &Mesh{C.NewtonMeshCreateFirstSingleSegment(m.handle)}
+}
+
+func (m *Mesh) CreateNextSingleSegment(segment *Mesh) *Mesh {
+	return &Mesh{C.NewtonMeshCreateNextSingleSegment(m.handle, segment.handle)}
+}
+
+func (m *Mesh) CreateFirstLayer() *Mesh {
+	return &Mesh{C.NewtonMeshCreateFirstLayer(m.handle)}
+}
+
+func (m *Mesh) CreateNextLayer(segment *Mesh) *Mesh {
+	return &Mesh{C.NewtonMeshCreateNextLayer(m.handle, segment.handle)}
+}
+
+func (m *Mesh) TotalFaceCount() int {
+	return int(C.NewtonMeshGetTotalFaceCount(m.handle))
+}
+
+func (m *Mesh) TotalIndexCount() int {
+	return int(C.NewtonMeshGetTotalIndexCount(m.handle))
+}
+
+func (m *Mesh) Faces(faceIndexCount, faceMaterial, faceIndices []int) {
+	//untested
+	C.NewtonMeshGetFaces(m.handle, (*C.int)(unsafe.Pointer(&faceIndexCount[0])),
+		(*C.int)(unsafe.Pointer(&faceMaterial[0])),
+		(*unsafe.Pointer)(unsafe.Pointer(&faceIndices[0])))
+}
+
+func (m *Mesh) PointCount() int {
+	return int(C.NewtonMeshGetPointCount(m.handle))
+}
+
+func (m *Mesh) PointStrideInByte() int {
+	return int(C.NewtonMeshGetPointStrideInByte(m.handle))
+}
+
+func (m *Mesh) PointArray(array []float64) {
+	cArray := C.NewtonMeshGetPointArray(m.handle)
+	C.CopyFloat64Array(cArray, (*C.dFloat64)(&array[0]), C.int(len(array)))
+}
+
+func (m *Mesh) NormalArray(array []float64) {
+	cArray := C.NewtonMeshGetNormalArray(m.handle)
+	C.CopyFloat64Array(cArray, (*C.dFloat64)(&array[0]), C.int(len(array)))
+}
+
+func (m *Mesh) UV0Array(array []float64) {
+	cArray := C.NewtonMeshGetUV0Array(m.handle)
+	C.CopyFloat64Array(cArray, (*C.dFloat64)(&array[0]), C.int(len(array)))
+}
+
+func (m *Mesh) UV1Array(array []float64) {
+	cArray := C.NewtonMeshGetUV1Array(m.handle)
+	C.CopyFloat64Array(cArray, (*C.dFloat64)(&array[0]), C.int(len(array)))
+}
+
+func (m *Mesh) VertexCount() int {
+	return int(C.NewtonMeshGetVertexCount(m.handle))
+}
+
+func (m *Mesh) VertexStrideInByte() int {
+	return int(C.NewtonMeshGetVertexStrideInByte(m.handle))
+}
+
+func (m *Mesh) VertexArray(array []float64) {
+	cArray := C.NewtonMeshGetVertexArray(m.handle)
+	C.CopyFloat64Array(cArray, (*C.dFloat64)(&array[0]), C.int(len(array)))
+}
+
+type MeshVertex struct {
+	handle unsafe.Pointer
+}
+
+func (m *Mesh) FirstVertex() *MeshVertex {
+	return &MeshVertex{C.NewtonMeshGetFirstVertex(m.handle)}
+}
+
+func (m *Mesh) NextVertex(vertex *MeshVertex) *MeshVertex {
+	return &MeshVertex{C.NewtonMeshGetNextVertex(m.handle, vertex.handle)}
+}
+
+func (m *Mesh) VertexIndex(vertex *MeshVertex) int {
+	return int(C.NewtonMeshGetVertexIndex(m.handle, vertex.handle))
+}
+
+type MeshPoint struct {
+	handle unsafe.Pointer
+}
+
+func (m *Mesh) FirstPoint() *MeshPoint {
+	return &MeshPoint{C.NewtonMeshGetFirstPoint(m.handle)}
+}
+
+func (m *Mesh) NextPoint(Point *MeshPoint) *MeshPoint {
+	return &MeshPoint{C.NewtonMeshGetNextPoint(m.handle, Point.handle)}
+}
+
+func (m *Mesh) PointIndex(Point *MeshPoint) int {
+	return int(C.NewtonMeshGetPointIndex(m.handle, Point.handle))
+}
+
+func (m *Mesh) VertexIndexFromPoint(point *MeshPoint) int {
+	return int(C.NewtonMeshGetVertexIndexFromPoint(m.handle, point.handle))
+}
+
+type MeshEdge struct {
+	handle unsafe.Pointer
+}
+
+func (m *Mesh) FirstEdge() *MeshEdge {
+	return &MeshEdge{C.NewtonMeshGetFirstEdge(m.handle)}
+}
+
+func (m *Mesh) NextEdge(Edge *MeshEdge) *MeshEdge {
+	return &MeshEdge{C.NewtonMeshGetNextEdge(m.handle, Edge.handle)}
+}
+
+func (m *Mesh) EdgeIndices(edge *MeshEdge, v0, v1 []int) {
+	C.NewtonMeshGetEdgeIndices(m.handle, edge.handle, (*C.int)(unsafe.Pointer(&v0[0])),
+		(*C.int)(unsafe.Pointer(&v1[0])))
+}
+
+type MeshFace struct {
+	handle unsafe.Pointer
+}
+
+func (m *Mesh) FirstFace() *MeshFace {
+	return &MeshFace{C.NewtonMeshGetFirstFace(m.handle)}
+}
+
+func (m *Mesh) NextFace(Face *MeshFace) *MeshFace {
+	return &MeshFace{C.NewtonMeshGetNextFace(m.handle, Face.handle)}
+}
+
+func (m *Mesh) IsFaceOpen(face *MeshFace) bool {
+	return gbool[int(C.NewtonMeshIsFaceOpen(m.handle, face.handle))]
+}
+
+func (m *Mesh) FaceMaterial(face *MeshFace) int {
+	return int(C.NewtonMeshGetFaceMaterial(m.handle, face.handle))
+}
+
+func (m *Mesh) FaceIndexCount(face *MeshFace) int {
+	return int(C.NewtonMeshGetFaceIndexCount(m.handle, face.handle))
+}
+
+func (m *Mesh) FaceIndices(Face *MeshFace, indices []int) {
+	C.NewtonMeshGetFaceIndices(m.handle, Face.handle, (*C.int)(unsafe.Pointer(&indices[0])))
+}
+
+func (m *Mesh) FacePointIndices(Face *MeshFace, indices []int) {
+	C.NewtonMeshGetFacePointIndices(m.handle, Face.handle, (*C.int)(unsafe.Pointer(&indices[0])))
+}
+
+func (m *Mesh) CalculateFaceNormal(face *MeshFace, normal []float64) {
+	C.NewtonMeshCalculateFaceNormal(m.handle, face.handle, (*C.dFloat64)(&normal[0]))
+}
+
+func (m *Mesh) SetFaceMaterial(face *MeshFace, matId int) {
+	C.NewtonMeshSetFaceMaterial(m.handle, face.handle, C.int(matId))
+}
